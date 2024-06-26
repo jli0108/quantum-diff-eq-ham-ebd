df23fc2baa4fc9293ca78aaa019248a116af325f
jli0108 <jli0108@umd.edu>
2024-06-26 12:19:26 -0400

    only compute gate counts per trotter step once

--- src/resource_analysis/fig2.py
+++ src/resource_analysis/fig2.py
@@ -165,7 +165,6 @@ def one_hot_gate_count_per_trotter_step(N, n_p, R, trotter_method="second_order"
 
     # Gates per Trotter step
     num_single_qubit_gates, num_two_qubit_gates = tket_circuit.n_1qb_gates(), tket_circuit.n_2qb_gates()
-    print(f"1q gates: {num_single_qubit_gates}, 2q gates: {num_two_qubit_gates}")
 
     return num_single_qubit_gates, num_two_qubit_gates
 
@@ -418,10 +417,10 @@ def bell_basis_gate_count_per_trotter_step(n_x, n_p, R, dt):
 
 if __name__ == "__main__":
 
-    print("Running Fig 2 script")
+    print("Running Fig 2 script", flush=True)
     start_time = time()
     dimensions = np.arange(1, 10)
-    error_tols = 1 / np.linspace(10, 10000, 10)
+    error_tols = 1 / np.linspace(10, 1000, 10)
     N = 128                                     # grid points along each dimension
     n_x = int(np.log2(N))
     n_p = 5                                     # num qubits for p
@@ -429,44 +428,44 @@ if __name__ == "__main__":
     T = 1
     R = 8
     num_samples = 1000
-    num_jobs = 32
+    num_jobs = 16
     trotter_method="second_order"
 
     print("dimensions:", dimensions)
     print("error tolerances:", error_tols)
 
-    bell_basis_1q_gate_count = np.zeros((len(dimensions), len(error_tols)))
-    bell_basis_2q_gate_count = np.zeros((len(dimensions), len(error_tols)))
-    one_hot_1q_gate_count = np.zeros((len(dimensions), len(error_tols)))
-    one_hot_2q_gate_count = np.zeros((len(dimensions), len(error_tols)))
+    bell_basis_trotter_steps = np.zeros((len(dimensions), len(error_tols)))
+    one_hot_trotter_steps = np.zeros((len(dimensions), len(error_tols)))
+
+    # Gate counts per Trotter step
+    bell_basis_single_qubit_gates, bell_basis_two_qubit_gates = bell_basis_gate_count_per_trotter_step(n_x, n_p, R, T)
+    one_hot_single_qubit_gates, one_hot_two_qubit_gates = one_hot_gate_count_per_trotter_step(N, n_p, R, T)
 
     for dim_idx, dimension in enumerate(dimensions):
         for error_tol_idx, error_tol in enumerate(error_tols):
-            print(f"Estimating gate counts for dimension {dimension}, error_tol={error_tol:0.2e}")
+            print(f"Estimating gate counts for dimension {dimension}, error_tol={error_tol:0.2e}", flush=True)
 
             '''Schrodingerization w/ Bell basis'''
-            r = get_trotter_number_bell_basis(n_x, n_p, R, T, error_tol / dimension, num_samples, num_jobs)
-            single_qubit_gates, two_qubit_gates = bell_basis_gate_count_per_trotter_step(n_x, n_p, R, T / r)
-            bell_basis_1q_gate_count[dim_idx, error_tol_idx] = r * single_qubit_gates
-            bell_basis_2q_gate_count[dim_idx, error_tol_idx] = r * two_qubit_gates
-
+            bell_basis_trotter_steps[dim_idx, error_tol_idx] = get_trotter_number_bell_basis(np.log2(N), T, error_tol=error_tol / dimension, periodic=True)
+            print("Bell basis Trotter steps:", bell_basis_trotter_steps[dim_idx, error_tol_idx], flush=True)
+            
             '''One-hot encoding (ours)'''
-            r = get_trotter_number_one_hot(N, N_p, R, T, error_tol / dimension, num_samples, num_jobs)
-            single_qubit_gates, two_qubit_gates = one_hot_gate_count_per_trotter_step(N, n_p, R, trotter_method)
-            one_hot_1q_gate_count[dim_idx, error_tol_idx] = r * single_qubit_gates
-            one_hot_2q_gate_count[dim_idx, error_tol_idx] = r * two_qubit_gates
+            one_hot_trotter_steps[dim_idx, error_tol_idx] = get_trotter_number_one_hot(N, N_p, R, T, error_tol / dimension, num_samples, num_jobs)
+            print("One-hot Trotter steps:", one_hot_trotter_steps[dim_idx, error_tol_idx], flush=True)
 
-            np.savez(join("../resource_analysis_data", "fig2_data.npz"),
-                    dimensions=dimensions,
-                    error_tols=error_tols,
-                    bell_basis_1q_gate_count=bell_basis_1q_gate_count,
-                    bell_basis_2q_gate_count=bell_basis_2q_gate_count,
-                    one_hot_1q_gate_count=one_hot_1q_gate_count,
-                    one_hot_2q_gate_count=one_hot_2q_gate_count)
+    np.savez(join("../resource_analysis_data", "fig2_data.npz"),
+            dimensions=dimensions,
+            error_tols=error_tols,
+            bell_basis_trotter_steps=bell_basis_trotter_steps,
+            bell_basis_single_qubit_gates=bell_basis_single_qubit_gates,
+            bell_basis_two_qubit_gates=bell_basis_two_qubit_gates,
+            one_hot_trotter_steps=one_hot_trotter_steps,
+            one_hot_single_qubit_gates=one_hot_single_qubit_gates,
+            one_hot_two_qubit_gates=one_hot_two_qubit_gates)
 
     end_time = time()
-    print(f"Runtime: {end_time - start_time}")
+    print(f"Runtime: {end_time - start_time}", flush=True)
 
-    print("Finished!")
+    print("Finished!", flush=True)
 
 
--- src/resource_analysis/script_fig2.sh
+++ src/resource_analysis/script_fig2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --output=fig2_output.out
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 #SBATCH -t 72:00:00
 #SBATCH --partition=serial
 
