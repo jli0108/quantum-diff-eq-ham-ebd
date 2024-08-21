93c0dd348d42db2622c748479970b959e5390122
jli0108 <jli0108@umd.edu>
2024-08-20 21:29:37 -0400

    keep first order trotter only

--- src/resource_analysis/fig2_nonseparable_first_order.py
+++ src/resource_analysis/fig2_nonseparable_first_order.py
@@ -1,7 +1,7 @@
-
 import numpy as np
 
 from scipy.sparse import eye, lil_matrix, diags
+from scipy.linalg.interpolative import estimate_spectral_norm
 from joblib import Parallel, delayed
 from resource_estimate_utils import *
 from os.path import join
@@ -12,14 +12,12 @@ from qiskit.quantum_info import SparsePauliOp
 from qiskit.synthesis import LieTrotter, SuzukiTrotter
 from qiskit import transpile
 from qiskit.circuit.library import PauliEvolutionGate
-# from pytket import OpType
-# from pytket.passes import RemoveRedundancies, CommuteThroughMultis, SequencePass, FullPeepholeOptimise, auto_rebase_pass
-# from pytket.extensions.qiskit import qiskit_to_tk
 
 from os.path import join
 import sys
 sys.path.append(join(".", ".."))
 from utils import *
+import resource
 
 def get_xi_pauli_op(n_p, R):
     xi_pauli_list = []
@@ -58,25 +56,63 @@ def get_H_std_binary(N, n_p, R):
 
     H_1_pauli_op_grouped = SparsePauliOp.from_operator(H_1.toarray()).group_commuting()
     H_2_pauli_op_grouped = SparsePauliOp.from_operator(H_2.toarray()).group_commuting()
+    
     D_pauli_op = SparsePauliOp.from_operator(D.toarray())
     H_F_pauli_op = (-get_xi_pauli_op(n_p, R))
     Id_n_p = SparsePauliOp.from_list([(n_p * 'I', 1)])
 
     H = []
+    H_sp_mats = []
 
     # Hermitian part
     for j in range(len(H_1_pauli_op_grouped)):
         H.append(H_1_pauli_op_grouped[j].tensor(D_pauli_op).tensor(H_F_pauli_op))
+        H_sp_mats.append(tensor([H_1_pauli_op_grouped[j].to_matrix(sparse=True), D, H_F_pauli_op.to_matrix(sparse=True)]))
         H.append(D_pauli_op.tensor(H_1_pauli_op_grouped[j]).tensor(H_F_pauli_op))
+        H_sp_mats.append(tensor([D, H_1_pauli_op_grouped[j].to_matrix(sparse=True), H_F_pauli_op.to_matrix(sparse=True)]))
     # Anti-Hermitian part
     for j in range(len(H_2_pauli_op_grouped)):
         H.append(H_2_pauli_op_grouped[j].tensor(D_pauli_op).tensor(Id_n_p))
+        H_sp_mats.append(tensor([H_2_pauli_op_grouped[j].to_matrix(sparse=True), D, identity(N_p)]))
         H.append(D_pauli_op.tensor(H_2_pauli_op_grouped[j]).tensor(Id_n_p))
+        H_sp_mats.append(tensor([D, H_2_pauli_op_grouped[j].to_matrix(sparse=True), identity(N_p)]))
 
-    return H
+    return H, H_sp_mats
+
+def one_hot_projection(H_term):
+    N = H_term.num_qubits
+    pauli_str = H_term.to_list()[0][0]
+    pauli_indices = []
+    for j in range(N):
+        if pauli_str[j] != 'I':
+            pauli_indices.append(j)
+    if len(pauli_indices) == 0:
+        mat = np.identity(N, dtype=np.complex128)
+    elif len(pauli_indices) == 1:
+        mat = np.identity(N, dtype=np.complex128)
+        assert pauli_str[pauli_indices[0]] == 'Z'
+        mat[pauli_indices[0], pauli_indices[0]] = -1
+    elif len(pauli_indices) == 2:
+        mat = np.zeros((N,N), dtype=np.complex128)
+        if pauli_str[pauli_indices[0]] + pauli_str[pauli_indices[1]] == 'XX' or pauli_str[pauli_indices[0]] + pauli_str[pauli_indices[1]] == 'YY':
+            mat[pauli_indices[0], pauli_indices[1]] = 1
+            mat[pauli_indices[1], pauli_indices[0]] = 1
+        elif pauli_str[pauli_indices[0]] + pauli_str[pauli_indices[1]] == 'XY':
+            mat[pauli_indices[0], pauli_indices[1]] *= -1j
+            mat[pauli_indices[1], pauli_indices[0]] *= 1j
+        elif pauli_str[pauli_indices[0]] + pauli_str[pauli_indices[1]] == 'YX':
+            mat[pauli_indices[0], pauli_indices[1]] *= -1j
+            mat[pauli_indices[1], pauli_indices[0]] *= 1j
+        else:
+            raise Exception()
+    else:
+        raise Exception()
+    return H_term.coeffs[0] * csc_matrix(mat)
 
 def get_H_one_hot(N, n_p, R):
     h = 1 / N
+    N_p = 2 ** n_p
+
     A = lil_matrix((N, N), dtype=np.complex128)
     for j in range(N):
         x = j / N
@@ -97,7 +133,6 @@ def get_H_one_hot(N, n_p, R):
     H_2_pauli_list = []
     D_pauli_list = []
 
-    H_1_pauli_list.append((N * 'I', H_1[0,0].real))
     for j in range(N):
         op = N * ['I']
         op[j] = 'X'
@@ -108,6 +143,11 @@ def get_H_one_hot(N, n_p, R):
         op[j] = 'Y'
         op[(j+1)%N] = 'Y'
         H_1_pauli_list.append((''.join(op), 0.5*H_1[(j+1)%N,j].real))
+
+        H_1_pauli_list.append((N * 'I', 0.5 * H_1[j,j].real))
+        op = N * ['I']
+        op[j] = 'Z'
+        H_1_pauli_list.append((''.join(op), -0.5 * H_1[j,j].real))
 
     for j in range(N):
         op = N * ['I']
@@ -119,6 +159,11 @@ def get_H_one_hot(N, n_p, R):
         op[j] = 'Y'
         op[(j+1)%N] = 'X'
         H_2_pauli_list.append((''.join(op), -0.5*H_2[(j+1)%N,j].imag))
+
+        H_2_pauli_list.append((N * 'I', 0.5 * H_2[j,j].real))
+        op = N * ['I']
+        op[j] = 'Z'
+        H_2_pauli_list.append((''.join(op), -0.5 * H_2[j,j].real))
 
     for j in range(N):
         D_pauli_list.append((N * 'I', 0.5 * D[j,j].real))
@@ -132,58 +177,50 @@ def get_H_one_hot(N, n_p, R):
     H_F_pauli_op = (-get_xi_pauli_op(n_p, R))
     Id_n_p = SparsePauliOp.from_list([(n_p * 'I', 1)])
     
-    H = []
+    H_1_sp_mats = []
+    for j in range(len(H_1_pauli_op_grouped)):
+        sp_mat = csc_matrix((N,N), dtype=np.complex128)
+        for k in range(len(H_1_pauli_op_grouped[j])):
+            sp_mat += one_hot_projection(H_1_pauli_op_grouped[j][k])
+        H_1_sp_mats.append(sp_mat)
+    H_2_sp_mats = []
+    for j in range(len(H_2_pauli_op_grouped)):
+        sp_mat = csc_matrix((N,N), dtype=np.complex128)
+        for k in range(len(H_2_pauli_op_grouped[j])):
+            sp_mat += one_hot_projection(H_2_pauli_op_grouped[j][k])
+        H_2_sp_mats.append(sp_mat)
 
+    H = []
+    H_sp_mats = []
     # Hermitian part
     for j in range(len(H_1_pauli_op_grouped)):
         H.append(H_1_pauli_op_grouped[j].tensor(D_pauli_op).tensor(H_F_pauli_op))
+        H_sp_mats.append(tensor([H_1_sp_mats[j], D, H_F_pauli_op.to_matrix(sparse=True)]))
         H.append(D_pauli_op.tensor(H_1_pauli_op_grouped[j]).tensor(H_F_pauli_op))
+        H_sp_mats.append(tensor([D, H_1_sp_mats[j], H_F_pauli_op.to_matrix(sparse=True)]))
     # Anti-Hermitian part
     for j in range(len(H_2_pauli_op_grouped)):
         H.append(H_2_pauli_op_grouped[j].tensor(D_pauli_op).tensor(Id_n_p))
+        H_sp_mats.append(tensor([H_2_sp_mats[j], D, identity(N_p)]))
         H.append(D_pauli_op.tensor(H_2_pauli_op_grouped[j]).tensor(Id_n_p))
+        H_sp_mats.append(tensor([D, H_2_sp_mats[j], identity(N_p)]))
 
-    return H
-
-# def get_gate_count_per_trotter_step_std_binary(H, trotter_method="second_order"):
-#     '''Constructs the full circuit for a single Trotter step'''
-#     # Compute number of gates per Trotter step
-#     if trotter_method == "first_order" or trotter_method == "randomized_first_order":
-#         circuit = LieTrotter(reps=1).synthesize(PauliEvolutionGate(H.simplify()))
-#     elif trotter_method == "second_order":
-#         circuit = SuzukiTrotter(order=2, reps=1).synthesize(PauliEvolutionGate(H.simplify()))
-#     else:
-#         raise ValueError(f"{trotter_method} not supported")
-
-#     compiled_circuit = transpile(circuit, basis_gates=['rxx', 'rx', 'ry', 'rz'], optimization_level=3)
-#     print(compiled_circuit.count_ops())
-
-#     tket_circuit = qiskit_to_tk(compiled_circuit)
-#     gateset = {OpType.Rx, OpType.Ry, OpType.Rz, OpType.XXPhase}
-#     rebase = auto_rebase_pass(gateset) 
-#     comp = SequencePass([FullPeepholeOptimise(), CommuteThroughMultis(), RemoveRedundancies(), rebase])
-#     comp.apply(tket_circuit)
-
-#     # Gates per Trotter step
-#     num_single_qubit_gates, num_two_qubit_gates = tket_circuit.n_1qb_gates(), tket_circuit.n_2qb_gates()
-
-#     return num_single_qubit_gates, num_two_qubit_gates
+    return H, H_sp_mats
 
 def get_gate_count_per_trotter_step(H, trotter_method="second_order"):
     num_single_qubit_gates, num_two_qubit_gates = 0, 0
     L = len(H)
     for j in range(L):
-        if j % 1000 == 0:
-            print(f"Getting gate counts per Trotter step: [{j}/{L}]", end="\r")
-
-        circuit = LieTrotter(reps=1).synthesize(PauliEvolutionGate(H[j]))
-        compiled_circuit = transpile(circuit, basis_gates=['rxx', 'rx', 'ry', 'rz'], optimization_level=3)
-        ops = compiled_circuit.count_ops()
-        for key in ops.keys():
-            if key == 'rx' or key == 'ry' or key == 'rz':
-                num_single_qubit_gates += ops[key]
-            elif key == 'rxx':
-                num_two_qubit_gates += ops[key]
+        print(f"Getting gate counts per Trotter step: [{j}/{L}]", end="\r")
+        for pauli_op in H[j]:
+            circuit = LieTrotter(reps=1).synthesize(PauliEvolutionGate(pauli_op))
+            compiled_circuit = transpile(circuit, basis_gates=['rxx', 'rx', 'ry', 'rz'], optimization_level=3)
+            ops = compiled_circuit.count_ops()
+            for key in ops.keys():
+                if key == 'rx' or key == 'ry' or key == 'rz':
+                    num_single_qubit_gates += ops[key]
+                elif key == 'rxx':
+                    num_two_qubit_gates += ops[key]
     
     print(f"Getting gate counts per Trotter step: [{L}/{L}]")
     if trotter_method == "first_order":
@@ -191,50 +228,39 @@ def get_gate_count_per_trotter_step(H, trotter_method="second_order"):
     elif trotter_method == "second_order":
         return 2 * num_single_qubit_gates, 2 * num_two_qubit_gates
     
-def accumulator_sum(generator):
-    result = 0
-    for value in generator:
-        result += value
-    return result
+def sparse_comm(H1, H2):
+    return H1 @ H2 - H2 @ H1
 
-def get_first_order_trotter_steps(t, H, epsilon, n_jobs=16):
+def get_first_order_trotter_steps(t, H, epsilon):
     L = len(H)
-    res = Parallel(n_jobs=n_jobs, return_as="generator")(delayed(get_comm_term_first_order)(H, L, j) for j in range(L-1))
-    coeff = accumulator_sum(res)
-
+    coeff = 0
+    for j in range(L-1):
+        print(f"Computing Trotter steps: [{j} / {L-1}]", end="\r", flush=True)
+        comm_term = 0
+        for k in np.arange(j+1, L):
+            comm_term += sparse_comm(H[j], H[k])
+        coeff += estimate_spectral_norm(comm_term)
+    print(f"Computing Trotter steps: [{L-1} / {L-1}]", flush=True)
     return np.ceil((t ** 2 * coeff / (2 * epsilon)))
 
-def get_comm_term_first_order(H, L, j):
-    comm_term = 0
-    for k in np.arange(j+1, L):
-        comm_term += commutator(H[k], H[j])
-    comm_term.simplify()
-    return np.linalg.norm(comm_term.coeffs, ord=1)
-
-
-def get_second_order_trotter_steps(t, H, epsilon, n_jobs=16):
+def get_second_order_trotter_steps(t, H, epsilon):
     L = len(H)
-    res = Parallel(n_jobs=n_jobs, return_as="generator")(delayed(get_comm_term_second_order)(H, L, j) for j in range(L-1))
-    coeff = accumulator_sum(res)
+    coeff = 0
+    for j in range(L-1):
+        print(f"Computing Trotter steps: [{j} / {L-1}]", end="\r", flush=True)
+        comm_term = 0
+        for k in np.arange(j+1, L):
+            for l in np.arange(j+1, L):
+                comm_term += sparse_comm(H[l], sparse_comm(H[k], H[j]))
+        coeff += estimate_spectral_norm(comm_term)
+
+        comm_term = 0
+        for k in np.arange(j+1, L):
+            comm_term += sparse_comm(H[j], sparse_comm(H[j], H[k]))
+        coeff += 0.5 * estimate_spectral_norm(comm_term)
+    print(f"Computing Trotter steps: [{L-1} / {L-1}]", flush=True)
 
     return np.ceil(np.sqrt(t ** 3 * coeff / (12 * epsilon)))
-
-def get_comm_term_second_order(H, L, j):
-    comm_term = 0
-    for k in np.arange(j+1, L):
-        for l in np.arange(j+1, L):
-            comm_term += commutator(H[l], commutator(H[k], H[j]))
-    comm_term.simplify()
-    coeff1 = np.linalg.norm(comm_term.coeffs, ord=1)
-
-    comm_term = 0
-    for k in np.arange(j+1, L):
-        comm_term += commutator(H[j], commutator(H[j], H[k]))
-    comm_term.simplify()
-    coeff2 = 0.5 * np.linalg.norm(comm_term.coeffs, ord=1)
-
-    return coeff1 + coeff2
-
 
 if __name__ == "__main__":
 
@@ -251,22 +277,26 @@ if __name__ == "__main__":
 
     print("Error tolerances:", error_tols, flush=True)
 
-    print("Computing Trotter steps.")
+    print("Computing Trotter steps.", flush=True)
     pauli_basis_trotter_steps = np.zeros((len(error_tols)))
     one_hot_trotter_steps = np.zeros((len(error_tols)))
 
     # Gate counts per Trotter step
-    H_std_binary = get_H_std_binary(N, n_p, R)
-    H_one_hot = get_H_one_hot(N, n_p, R)
+    print("Getting Hamiltonian w/ Pauli basis.", flush=True)
+    H_std_binary, H_std_binary_sp_mats = get_H_std_binary(N, n_p, R)
+    print("Computing Trotter steps for Pauli basis.", flush=True)
+    pauli_basis_trotter_steps = get_first_order_trotter_steps(T, H_std_binary_sp_mats, error_tols)
+    print("Computing gates per Trotter step for Pauli basis.", flush=True)
     pauli_basis_single_qubit_gates, pauli_basis_two_qubit_gates = get_gate_count_per_trotter_step(H_std_binary, trotter_method)
-    one_hot_single_qubit_gates, one_hot_two_qubit_gates = get_gate_count_per_trotter_step(H_one_hot, trotter_method)
-
-    print("Computing Trotter steps for Pauli basis.")
-    pauli_basis_trotter_steps = get_first_order_trotter_steps(T, H_std_binary, error_tols)
 
     '''One-hot encoding (ours)'''
-    print("Computing Trotter steps for one-hot encoding.")
-    one_hot_trotter_steps = get_first_order_trotter_steps(T, H_one_hot, error_tols)
+    print("Getting Hamiltonian w/ one-hot encoding.", flush=True)
+    H_one_hot, H_one_hot_sp_mats = get_H_one_hot(N, n_p, R)
+    print("Computing Trotter steps for one-hot encoding.", flush=True)
+    one_hot_trotter_steps = get_first_order_trotter_steps(T, H_one_hot_sp_mats, error_tols)
+    print("Computing gates per Trotter step for one-hot.", flush=True)
+    one_hot_single_qubit_gates, one_hot_two_qubit_gates = get_gate_count_per_trotter_step(H_one_hot, trotter_method)
+
 
     np.savez(join("../resource_analysis_data", f"fig2_nonseparable_data_{trotter_method}.npz"),
             error_tols=error_tols,
@@ -281,5 +311,5 @@ if __name__ == "__main__":
     print(f"Runtime: {end_time - start_time}", flush=True)
 
     print("Finished!", flush=True)
-
+    print("Memory usage:", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
 
