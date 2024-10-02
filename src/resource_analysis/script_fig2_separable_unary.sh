408de94b0455422eb757c8a9bb5e9a5e8de3d2fb
jli0108 <jli0108@umd.edu>
2024-10-02 11:14:27 -0400

    delete files

--- src/resource_analysis/script_fig2_separable_unary.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --output=fig2_separable_unary_output.out
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH -t 72:00:00
-#SBATCH --partition=serial
-
-python fig2_separable_unary.py
