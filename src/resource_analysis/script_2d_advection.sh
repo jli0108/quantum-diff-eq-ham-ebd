a07c53bf289f3d76d2008e37ddb55ea75f6e44b5
jli0108 <jli0108@umd.edu>
2024-10-02 14:40:52 -0400

    remove file

--- src/resource_analysis/script_2d_advection.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --output=resource_analysis_2d_advection_output.out
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH -t 72:00:00
-#SBATCH --partition=serial
-
-python resource_analysis_2d_advection.py
